c5bd26a2aa8c238d051d5abda42a353ad5f2f9c5
MubasharAli7390 <MubasharAli7390@gmail.com>
2024-08-05 08:46:09 +0500

    change the switch statement to if-else statements beacuse they were causing problems added the ability the search the employee by different attributes added the ability to add a new employee added the ability to loop the program

--- main.cpp
+++ main.cpp
@@ -1,75 +1,148 @@
 #include<iostream>
 #include<string>
+#include<stdlib.h>
 using namespace std;
 
 
+string employeeName[100] = {"Mubashar", "Ali", "Ahmad"};
+string employeeID[100] = {"EMPLOYEE1", "EMPLOYEE2", "EMPLOYEE3"};
+string employeeDesignation[100] = {"Software-Engineer", "Jr.-Software-Engineer", "Sr.Software-Engineer"};
+int employeeSalary[100] = {50000, 35000, 80000};
+string employeePhone[100] = {"03134567890", "03000000000", "03987654321"};
+int arraySize = 100;
+
+
+void infoRetrieval(int indx);
+
+
 int main() {
+
     int firstOption;
-    int index = 1;
-    string employeeName[100] = {"Mubashar", "Ali", "Ahmad"};
-    string employeeID[100] = {"EMPLOYEE1", "EMPLOYEE2", "EMPLOYEE3"};
-    string employeeDesignation[100] = {"Software Engineer", "Jr. Software Engineer", "Sr.Software Engineer"};
-    int employeeSalary[100] = {50000, 35000, 80000};
-    string employeePhone[100] = {"03134567890", "03000000000", "03987654321"};
-    cout<<"Employee Management System"<<endl;
+    int index = 3;
+    int continueOrBreak;
+    int wait;
+
+    
+do {
+
+    cout<<"\nEmployee Management System"<<endl;
     cout<<"Please Select an option"<<endl;
     cout<<"1.Search Employee"<<endl;
     cout<<"2. Add Employee"<<endl;
     cin>>firstOption;
+    system("clear");
+
     
-    switch(firstOption) {
-        case 1:
+
+    if (firstOption == 1) {
+
         int searchOption;
         cout<<"Please Select an option"<<endl;
         cout<<"1.Search By Name"<<endl;
         cout<<"2.Search By ID"<<endl;
         cout<<"3.Search By Designation"<<endl;
         cout<<"4.Search By Salary"<<endl;
+        cout<<"5.Search By Phone Number"<<endl;
         cin>>searchOption;
+        system("clear");
 
-        switch(searchOption) {
-            case 1:
-            {
+        if (searchOption == 1) {
+            
             string name;
             cout<<"Enter Name of the Employee: ";
-            getline(cin, name);
-            cout<<name;
-            break;
-            };
-            case 2:
-            {
+            cin>>name;
+            for (int i = 0; i <= arraySize; i++) {
+                if (employeeName[i] == name) {
+                    infoRetrieval(i);
+                    break;
+
+                }
+            }
+
+        } else if (searchOption == 2) {
+
             string id;
-            cout<<"Enter ID of the Employee: ";
-            getline(cin, id);
-            cout<<id;
-            break;
-            };
-            case 3:
-            {
+            cout<<"Enter ID to search by: ";
+            cin>>id;
+
+            for (int i = 0; i <= arraySize; i++) {
+                if (employeeID[i] == id) {
+                    infoRetrieval(i);
+                    break;
+
+                }
+            }
+
+        } else if (searchOption == 3) {
+
             string designation;
             cout<<"Enter Designation to search: ";
-            getline(cin, designation);
-            cout<<designation;
-            break;
-            };
-            case 4:
-            {
+            cin>>designation;
+            for (int i = 0; i <= arraySize; i++) {
+                if (employeeDesignation[i] == designation) {
+                    infoRetrieval(i);
+                    break;
+
+                }
+            }
+
+            } else if (searchOption == 4) {
+
             int salary;
             cout<<"Enter salary to search for: ";
-            getline(cin, salary);
-            cout<<salary;
-            break;
-            };
-        }
-        break;
-        case 2:
-        cout<<"Add Employee";
-        break;
-    }
+            cin>>salary;
+            for (int i = 0; i <= arraySize; i++) {
+                if (employeeSalary[i] == salary) {
+                    infoRetrieval(i);
 
-   /* if (firstOption == 1) {
-        cout<<"Search Employee";
+                }
+            }
+
+        }  else if (searchOption == 5) {
+            string ph;
+            cout<<"Enter phone number to search by: ";
+            cin>>ph;
+            for (int i = 0; i <= arraySize; i++) {
+                if (employeePhone[i] == ph) {
+                    infoRetrieval(i);
+                    break;
+
+                }
+            }
+        
+        }
     } else if (firstOption == 2) {
-        cout<<"Add Employee";
-    } */
+
+        cout<<"Name: ";
+        cin>>employeeName[index];
+        cout<<"ID: ";
+        cin>>employeeID[index];
+        cout<<"Designation: ";
+        cin>>employeeDesignation[index];
+        cout<<"Salary: ";
+        cin>>employeeSalary[index];
+        cout<<"Phone Number: ";
+        cin>>employeePhone[index];
+        index++;
+        cout<<"Employeee added successfully"<<endl;
+        cout<<"\nEnter 1 to go back to main menu. 2 to close the program."<<endl;
+        cin>>continueOrBreak;
+        system("clear");
+        if (continueOrBreak == 2) {
+            cout<<"Thanks for using the program."<<endl;
+        }
+
+
+    }
+     } while (continueOrBreak == 1);
+    
+}
+
+
+void infoRetrieval(int indx) {
+    cout<<"Name: "<<employeeName[indx]<<endl;
+    cout<<"ID: "<<employeeID[indx]<<endl;
+    cout<<"Designation: "<<employeeDesignation[indx]<<endl;
+    cout<<"Salary: "<<employeeSalary[indx]<<endl;
+    cout<<"Phone Number: "<<employeePhone[indx]<<endl;
 }
